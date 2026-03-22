40eca9c648067da41546993f0727c97bb6ef6d8f
juandi247 <jdiaz873@unab.edu.co>
2026-03-22 02:49:51 +0100

    commentar con el mismito de Vs CODE (solo para juandi pc)

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -2,7 +2,8 @@ return {
     "numToStr/Comment.nvim",
     dependencies = {"nvim-treesitter/nvim-treesitter"},
     config = function() require("Comment").setup() 
- vim.keymap.set("n", "<C-}>", "gcc", { remap = true, desc = "Toggle comment" })
-  vim.keymap.set("v", "<C-}>", "gc", { remap = true, desc = "Toggle comment" })
+	 vim.keymap.set("n", "<C-]>", "gcc", { remap = true, desc = "Toggle comment" })
+  vim.keymap.set("v", "<C-]>", "gc", { remap = true, desc = "Toggle comment" })
     end
 }
+
