d8e96f3e4f3ada90542fb444c2e99682c3a4968f
juandi247 <jdiaz873@unab.edu.co>
2026-04-01 23:34:42 +0200

    numbersss

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 --GENERALL
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.opt.number = true
+vim.opt.relativenumber = true
 -- neo-tree
 vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
